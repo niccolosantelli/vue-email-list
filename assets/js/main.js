94450dd8e6b93b3dea1cdc8d109a24a568642f10
niccolosantelli <niccolo_santelli@live.it>
2021-01-08 15:12:30 +0100

    esercizio emails

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+let app = new Vue({
+  el:"#app",
+  data: {
+    emails: [],
+
+  },
+
+  mounted(){
+    for (let i = 0; i < 10; i++) {
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then(response => {
+        console.log(response.data.response);
+        this.emails.push(response.data.response);
+      })
+    }
+  }
+
+
+
+});
